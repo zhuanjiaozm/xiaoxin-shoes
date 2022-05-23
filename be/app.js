edf24699d6e9a25011fe9a43d8388459d22a0557
像风走了八千里，不问归期 <602763799@qq.com>
2022-05-23 17:47:12 +0800

    test cors

--- be/app.js
+++ be/app.js
@@ -14,7 +14,7 @@ const getItem = require('./2.fashiongo/getItem');
 const app = express()
 const port = 3000;
 
-app.use(cors())
+// app.use(cors())
 app.use(bodyParser.json())  // 解析json数据
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -22,15 +22,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.js
 
 app.all('*', function (req, res, next) {
-    let ol = config.url.cors.split(',');
-    if (ol.indexOf(req.headers.origin) >= 0) {
-        res.header("Access-Control-Allow-Origin", req.headers.origin);
-        res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
-        res.header("Access-Control-Allow-Credentials", "true");
-        res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-        res.header("X-Powered-By", ' Express 4.17.1');
-        res.header("Content-Type", "application/json;charset=utf-8");
-    }
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Content-Type', 'application/json;charset=utf-8');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
 });
 

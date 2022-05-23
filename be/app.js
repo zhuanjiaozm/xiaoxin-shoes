8e1f9ef28f237a8b1d700026d5fa4b7a7f5e912d
像风走了八千里，不问归期 <602763799@qq.com>
2022-05-23 17:49:56 +0800

    test cors

--- be/app.js
+++ be/app.js
@@ -14,20 +14,23 @@ const getItem = require('./2.fashiongo/getItem');
 const app = express()
 const port = 3000;
 
-// app.use(cors())
+var whitelist = ['http://localhost:3000', 'http://fengziqiao.xyz/']
+var corsOptions = {
+    credentials: true,
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json())  // 解析json数据
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-// app.js
-
-app.all('*', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    res.header('Content-Type', 'application/json;charset=utf-8');
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    next();
-});
 
 //2.注册路由模块
 app.use(router)
